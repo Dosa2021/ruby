7fe98471e55499fc92950ed0cea75cbe9c29ccfe
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-10-03 08:57:48 +0900

    メニューの一覧表示

--- index.rb
+++ index.rb
@@ -1,25 +1,10 @@
-# class Menu
-#     # NOTE: インスタンス変数
-#     attr_accessor :name
-#     attr_accessor :price
-
-#     def initialize(name: , price:)
-#         self.name = "ピザ"
-#     end
-
-#     # NOTE: インスタンスメソッド
-#     def show(data)
-#         puts "私は#{data}です"
-#     end
-
-#     def show_name
-#         puts "私は#{self.name}です"
-#     end
-# end
-
 require './menu'
 
-# menu1 = Menu.new
 menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "寿司", price: 1000)
 
-puts menu1.name
+menus = [menu1, menu2]
+
+menus.each do |menu|
+    puts "#{menu.name}: #{menu.price}円"
+end
